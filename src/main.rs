200fb8acda18309bbae76586ff5b6f6c26e58c00
Molecular Man <a.berezh@gmail.com>
2024-06-24 09:51:39 +0200

    implement correct zoom actions

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,6 @@ const APP_ID: &str = "com.andr2i.hallyview";
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
 
-    app.connect_open(move |app, files, _hint| {
-        app.activate();
-
-        dbg!(app.active_window());
-        dbg!(files);
-        //for file in files {
-        //    let path = file.path();
-        //    let doc = Document::from_file(&path, None).unwrap();
-        //    println!("Opened file: {}", path.display());
-        //}
-        //app.quit();
-    });
-
     app.connect_activate(build_ui);
     app.run()
 }
@@ -32,12 +19,12 @@ fn main() -> glib::ExitCode {
 fn build_ui(app: &Application) {
     let header_bar = gtk::HeaderBar::builder().build();
 
-    let open_button = gtk::Button::from_icon_name("document-open");
+    let open_button = Button::from_icon_name("document-open");
 
     header_bar.pack_start(&open_button);
 
-    let zoom_out_button = gtk::Button::from_icon_name("zoom-out");
-    let zoom_in_button = gtk::Button::from_icon_name("zoom-in");
+    let zoom_out_button = Button::from_icon_name("zoom-out");
+    let zoom_in_button = Button::from_icon_name("zoom-in");
 
     header_bar.pack_start(&zoom_out_button);
     header_bar.pack_start(&zoom_in_button);
@@ -92,21 +79,13 @@ fn build_ui(app: &Application) {
             pages_box.remove(&child);
         }
 
-        dbg!(doc.n_pages());
-
         for page_num in 0..doc.n_pages() {
             let page = doc.page(page_num).unwrap();
             let (width, height) = page.size();
 
-
-            dbg!(page.size());
-
             let drawing_area = DrawingArea::new();
             drawing_area.set_size_request(width as i32, height as i32);
             drawing_area.set_draw_func(clone!(@strong zoom => move |_, cr, _width, _height| {
-                dbg!(_width);
-                dbg!(_height);
-                dbg!(zoom.get());
                 cr.scale(zoom.get(), zoom.get());
                 page.render(cr);
             }));
@@ -131,89 +110,28 @@ fn build_ui(app: &Application) {
         }))
     }));
 
-    zoom_in_button.connect_clicked(clone!(@weak pages_box, @strong zoom => move |_| {
-        if let Some(first_page) = pages_box.first_child() {
-            zoom.set(zoom.get() * 1.1);
-            dbg!("clicked", zoom.get());
-            let page = first_page.downcast_ref::<DrawingArea>().unwrap();
+    let apply_zoom = clone!(@weak pages_box, @strong zoom => move |zoom_factor: f64| {
+        zoom.set(zoom.get() * zoom_factor);
+
+        let mut child = pages_box.first_child();
+
+        while let Some(c) = child {
+            let page = c.downcast_ref::<DrawingArea>().unwrap();
             let width = page.width();
             let height = page.height();
-            page.set_size_request((width as f64 * 1.1) as i32, (height as f64 * 1.1) as i32);
+            page.set_size_request((width as f64 * zoom_factor) as i32, (height as f64 * zoom_factor) as i32);
             page.queue_draw();
+            child = c.next_sibling();
         }
+    });
+
+    zoom_in_button.connect_clicked(clone!(@strong apply_zoom => move |_| {
+        apply_zoom(1.1);
+    }));
+
+    zoom_out_button.connect_clicked(clone!(@strong apply_zoom => move |_| {
+        apply_zoom(1./1.1);
     }));
 
     window.present();
 }
-
-fn example_main() -> glib::ExitCode {
-    // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
-
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_example_ui);
-
-    // Run the application
-    app.run()
-}
-
-fn build_example_ui(app: &Application) {
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(|button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
-    });
-
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
-        .child(&button)
-        .build();
-
-    // Present window
-    window.present();
-}
-
-//fn example_poppler_main() {
-//    let app = Application::builder()
-//        .application_id("com.example.pdfviewer")
-//        //.flags(
-//        //    gtk::gio::ApplicationFlags::HANDLES_OPEN
-//        //        | gtk::gio::ApplicationFlags::HANDLES_COMMAND_LINE,
-//        //)
-//        .build();
-//
-//    app.connect_activate(|app| {
-//        let window = ApplicationWindow::builder()
-//            .application(app)
-//            .title("PDF Viewer")
-//            .default_width(800)
-//            .default_height(600)
-//            .build();
-//
-//        let drawing_area = DrawingArea::new();
-//        drawing_area.set_draw_func(|_, cr, _width, _height| {
-//            // Render the PDF here
-//            if let Ok(document) = PopplerDocument::new_from_file("./test.pdf", None) {
-//                if let Some(page) = document.pages().next() {
-//                    page.render(cr);
-//                }
-//            }
-//        });
-//
-//        window.set_child(Some(&drawing_area));
-//        window.show();
-//    });
-//
-//    app.run();
-//}
